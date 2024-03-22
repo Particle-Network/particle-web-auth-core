8ec0ac13d736d989910f586936e1d8f302e709a2
Leo Wang <wangwenlong@minijoy.com>
2024-03-22 16:35:46 +0800

    feat: add isMounted

--- packages/next-web3modal-wagmi-starter/app/page.tsx
+++ packages/next-web3modal-wagmi-starter/app/page.tsx
@@ -1,39 +1,21 @@
 'use client';
 
 import ConnectButton from '@/connectButton';
-import { useEffect, useState } from 'react';
+import { useIsMounted } from '@particle-network/auth-core-modal';
 import { useAccount } from 'wagmi';
 import styles from './page.module.css';
-
-const waitForElement = (selector: string) => {
-    return new Promise((resolve) => {
-        const interval = setInterval(() => {
-            const element = document.querySelector(selector);
-            if (element) {
-                clearInterval(interval);
-                resolve(element);
-            }
-        }, 100);
-    });
-};
 
 export default function Home() {
     const { address, isConnected, chainId } = useAccount();
 
-    const [isLoaded, setIsLoaded] = useState(false);
-
-    useEffect(() => {
-        waitForElement('w3m-modal').then(() => {
-            setIsLoaded(true);
-        });
-    }, []);
+    const isMounted = useIsMounted();
 
     return (
         <main className={styles.main}>
             <div>
                 <ConnectButton />
             </div>
-            {isLoaded && isConnected && (
+            {isMounted && isConnected && (
                 <div className={styles.walletInfo}>
                     <div>Address: {address}</div>
                     <div>ChainId: {chainId}</div>
