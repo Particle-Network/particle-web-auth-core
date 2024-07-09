2d01ee9959e62d7d60d73c1f3109e73bdc357542
Leo Wang <wangwenlong@minijoy.com>
2024-07-09 16:16:44 +0800

    feat: add @particle-network/vite-plugin-wasm

--- packages/auth-core-vite-demo/vite.config.ts
+++ packages/auth-core-vite-demo/vite.config.ts
@@ -1,30 +1,9 @@
+import particleWasmPlugin from '@particle-network/vite-plugin-wasm';
 import vue from '@vitejs/plugin-vue';
-import fs from 'fs';
-import path from 'path';
-import type { ConfigEnv, Plugin } from 'vite';
 import { defineConfig } from 'vite';
 
-const particleWasmPlugin: Plugin | undefined = {
-    name: 'particle-wasm',
-    apply: (_, env: ConfigEnv) => {
-        return env.mode === 'development';
-    },
-    buildStart: () => {
-        const copiedPath = path.join(
-            __dirname,
-            '../../node_modules/@particle-network/thresh-sig/wasm/thresh_sig_wasm_bg.wasm' //@particle-network/thresh-sig dir
-        );
-        const dir = path.join(__dirname, 'node_modules/.vite/wasm');
-        const resultPath = path.join(dir, 'thresh_sig_wasm_bg.wasm');
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-        fs.copyFileSync(copiedPath, resultPath);
-    },
-};
-
 export default defineConfig({
-    plugins: [vue(), particleWasmPlugin],
+    plugins: [vue(), particleWasmPlugin()],
     resolve: {
         alias: {
             buffer: 'buffer',
--- packages/vite-react-starter/vite.config.ts
+++ packages/vite-react-starter/vite.config.ts
@@ -1,28 +1,8 @@
+import particleWasmPlugin from '@particle-network/vite-plugin-wasm';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import { ConfigEnv, Plugin, defineConfig } from 'vite';
-
-const particleWasmPlugin: Plugin | undefined = {
-    name: 'particle-wasm',
-    apply: (_, env: ConfigEnv) => {
-        return env.mode === 'development';
-    },
-    buildStart: () => {
-        const copiedPath = path.join(
-            __dirname,
-            '../../node_modules/@particle-network/thresh-sig/wasm/thresh_sig_wasm_bg.wasm' //@particle-network/thresh-sig dir
-        );
-        const dir = path.join(__dirname, 'node_modules/.vite/wasm');
-        const resultPath = path.join(dir, 'thresh_sig_wasm_bg.wasm');
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-        fs.copyFileSync(copiedPath, resultPath);
-    },
-};
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), particleWasmPlugin],
+    plugins: [react(), particleWasmPlugin()],
 });
